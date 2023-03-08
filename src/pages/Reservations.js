59b700d63587827443359a17c21ced3191e9f20d
Nick Swinford <nicholasjohn16@gmail.com>
2023-03-08 16:00:48 -0500

    add reservations page

--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,6 +1,42 @@
+import resturant from '../assets/restaurant.jpg';
+import Footer from '../components/Footer';
+import ReservationForm from '../components/ReservationForm';
+import '../assets/Reservations.css';
+
+const styles = {
+    header: {
+        backgroundImage: `url(${resturant})`,
+        minHeight: '30vh',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    h1: {
+        fontSize: '4rem',
+        margin: '0 20%',
+        color: '#FFF',
+        textShadow: '1px 1px 2px #000'
+    }
+}
+
 function Reservations() {
     return (
-        <h1>Reservations</h1>
+        <>
+            <div style={styles.header}>
+                <h1 style={styles.h1}>Reservations</h1>
+            </div>
+
+            <section className='reservation-form'>
+                <h2>Reserve a Table</h2>
+
+                <ReservationForm />
+            </section>
+
+            <Footer />
+        </>
     )
 }
 
